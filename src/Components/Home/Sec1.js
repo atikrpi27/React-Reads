1b7aec9c16c8d9b8718ca933325761a47e6a697e
Md. Atikur Rahman <58600348+atikrpi27@users.noreply.github.com>
2022-12-18 00:02:02 +0600

    setup environment and create a project

---
+++ src/Components/Home/Sec1.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sec1 = () => {
+  return (
+    <div>
+        <h1>Home page section 1</h1>
+    </div>
+  )
+}
+
+export default Sec1
---
+++ src/Pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sec1 from '../Components/Home/Sec1.js'
+
+const Home = () => {
+  return (
+    <>
+        <Sec1 />
+    </>
+  )
+}
+
+export default Home
