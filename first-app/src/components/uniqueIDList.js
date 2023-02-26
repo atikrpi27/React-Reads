e2401c2b99d594336087aa9c46940e63a2a5723e
Md. Atikur Rahman <58600348+atikrpi27@users.noreply.github.com>
2023-02-27 00:08:41 +0600

    Unique id

--- first-app/src/components/uniqueIDList.js
+++ first-app/src/components/uniqueIDList.js
@@ -1,16 +1,19 @@
-/* eslint-disable react/style-prop-object */
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 const todos = [
   {
+    "card_id": uuidv4(),
     "card_titles": "Json Data TITLE 1",
     "card_descriptions": "This title comes from Json Data. This title comes from Json Data. This title comes from Json Data. This title comes from Json Data. This title comes from Json Data."
   },
   {
+    "card_id": uuidv4(),
     "card_titles": "Json Data TITLE 2",
     "card_descriptions": "This title comes from Json Data. This title comes from Json Data. This title comes from Json Data. This title comes from Json Data. This title comes from Json Data."
   },
   {
+    "card_id": uuidv4(),
     "card_titles": "Json Data TITLE 3",
     "card_descriptions": "This title comes from Json Data. This title comes from Json Data. This title comes from Json Data. This title comes from Json Data. This title comes from Json Data."
   }
@@ -19,8 +22,11 @@ const todos = [
 const uniqueIDList = () => {
   return <div className='uniqueList'>
     {todos.map((todo) => {
-      return <div>
-        <h3>todo.card_titles</h3>
+      return <div key={uuidv4()}>
+        <h4>{todo.card_titles}</h4>
+        <p>{"Card Unique ID: " + todo.card_id}</p>
+        <p>{todo.card_descriptions}</p>
+        <br></br>
       </div>
     })}
     </div>
